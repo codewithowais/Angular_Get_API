3a37a3133a5b8bc20e0691e5d2de4e1d320a8aac
Muhammad Owais Ahmed <owaisraza2051@gmail.com>
2022-11-05 17:26:12 +0500

    work for image angular fb image grid

---
+++ src/app/Components/image-library/image-library.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-image-library',
+  templateUrl: './image-library.component.html',
+  styleUrls: ['./image-library.component.scss']
+})
+export class ImageLibraryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserScreenComponent } from './Components/user-screen/user-screen.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ImageLibraryComponent } from './Components/image-library/image-library.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserScreenComponent
+    UserScreenComponent,
+    ImageLibraryComponent
   ],
   imports: [
     BrowserModule,
